09eddcbc27a6d90dc3654e1bf80dfb69a39f0857
Abdul Miah <miaham@cardiff.ac.uk>
2021-11-17 10:58:01 +0000

    Initialise project

---
+++ src/main/java/cf/ac/uk/wrackreport/WrackReportApplication.java
@@ -0,0 +1,13 @@
+package cf.ac.uk.wrackreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrackReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrackReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cf/ac/uk/wrackreport/WrackReportApplicationTests.java
@@ -0,0 +1,13 @@
+package cf.ac.uk.wrackreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrackReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
