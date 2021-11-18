98e83e529320dad7c31df51ab92261476cdb612d
Abdul Miah <miaham@cardiff.ac.uk>
2021-11-18 16:34:20 +0000

    Created the JPA repository so that we can create sql statements to retrieve data from the database. Added a saveReport method that will act as an Insert statement, which will insert new data into the reports table

---
+++ src/main/java/cf/ac/uk/wrackreport/data/jpa/adaptors/WrackReportRepositoryAdaptor.java
@@ -0,0 +1,24 @@
+package cf.ac.uk.wrackreport.data.jpa.adaptors;
+
+import cf.ac.uk.wrackreport.data.interfaces.WrackReportRepository;
+import cf.ac.uk.wrackreport.data.jpa.entities.ReportEntity;
+import cf.ac.uk.wrackreport.data.jpa.repositories.ReportRepository;
+import cf.ac.uk.wrackreport.domain.Report;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+public class WrackReportRepositoryAdaptor implements WrackReportRepository {
+
+    private ReportRepository reportRepository;
+
+    public void ReportRepository(ReportRepository repo) {
+        reportRepository = repo;
+    }
+
+    public void saveReport(Report aReport) {
+        ReportEntity reportEntity = new ReportEntity(aReport);
+        reportRepository.save(reportEntity);
+    }
+}
--- src/main/java/cf/ac/uk/wrackreport/data/jpa/entities/ReportEntity.java
+++ src/main/java/cf/ac/uk/wrackreport/data/jpa/entities/ReportEntity.java
@@ -1,5 +1,6 @@
 package cf.ac.uk.wrackreport.data.jpa.entities;
 
+import cf.ac.uk.wrackreport.domain.Report;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -35,8 +36,18 @@ public class ReportEntity {
     @Column(name = "postcode")
     private String postcode;
 
-    public cf.ac.uk.wrackreport.domain.Report toDomain() {
-        cf.ac.uk.wrackreport.domain.Report domainReport = new cf.ac.uk.wrackreport.domain.Report (
+    public ReportEntity(Report aReport) {
+        this.reportId = aReport.getReportId();
+        this.userId = aReport.getUserId();
+        this.categoryId = aReport.getCategoryId();
+        this.description = aReport.getDescription();
+        this.latLong = aReport.getLatLong();
+        this.datetime = aReport.getDatetime();
+        this.postcode = aReport.getPostcode();
+    }
+
+    public Report toDomain() {
+        Report domainReport = new Report (
                 this.reportId,
                 this.userId,
                 this.categoryId,
