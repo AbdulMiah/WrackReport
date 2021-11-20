c83da187e827159fb3881c8eb7fa26daa2e73cc7
Oliver <starsky135@gmail.com>
2021-11-20 02:08:16 +0000

    Modified to use constructor-based dependency injection

--- src/main/java/cf/ac/uk/wrackreport/web/controllers/ReportController.java
+++ src/main/java/cf/ac/uk/wrackreport/web/controllers/ReportController.java
@@ -15,8 +15,11 @@ import javax.validation.Valid;
 @Controller
 public class ReportController {
 
-    @Autowired
     private ReportService reportService;
+
+    public ReportController(ReportService reportService){
+        this.reportService = reportService;
+    }
 
     @GetMapping("/report-form")
     public String displayReportForm(Model model) {
