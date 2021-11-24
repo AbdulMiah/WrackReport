a086d9a5539788377210b488d631cdcd3f8c3d24
Abdul Miah <miaham@cardiff.ac.uk>
2021-11-24 14:28:38 +0000

    Created the converter from inches and centimeters to meters. Conversion is done client-side in javascript.

--- src/main/resources/static/js/report-form.js
+++ src/main/resources/static/js/report-form.js
@@ -2,6 +2,8 @@ const description = document.getElementById('description');
 const remainingCharsText = document.getElementById('remaining-chars');
 const seeManualDepthEntry = document.getElementById('seeManualDepthEntry');
 const manualDepthEntryDiv = document.getElementById('manualDepthEntry');
+const measurementType = document.getElementById('measurementType');
+
 const MAX_Chars = 2500;
 
 if(description != null){
@@ -25,10 +27,29 @@ else{
 }
 
 function hideManualDepthEntry() {
-   var value = seeManualDepthEntry.options[seeManualDepthEntry.selectedIndex].text;
+   const value = seeManualDepthEntry.options[seeManualDepthEntry.selectedIndex].text;
    if (value == "Other" && manualDepthEntryDiv.style.display==="none") {
       manualDepthEntryDiv.style.display = "block";
    } else {
       manualDepthEntryDiv.style.display = "none";
    }
+}
+
+function lengthConverter() {
+   const measureType = measurementType.options[measurementType.selectedIndex].text;
+   let depthMeterField = document.getElementById('depthMeterField').value;
+   let convertedValue = document.getElementById('convertedVal');
+   console.log(depthMeterField);
+   console.log(measureType);
+
+      if (measureType == "Inches") {
+         const cm = depthMeterField / 0.39370;
+         const inchesToMeters = cm / 100;
+         console.log(inchesToMeters);
+         convertedValue.value=inchesToMeters;
+      } else {
+         const cmToMeters = depthMeterField / 100
+         console.log(cmToMeters);
+         convertedValue.value=cmToMeters;
+      }
 }
