e87a18a2e9b672c095311465f95b711dccef6c7d
c2061455 <okeefesj@cardiff.ac.uk>
2021-11-25 15:43:23 +0000

    Added name fields for files

--- src/main/resources/static/js/report-form.js
+++ src/main/resources/static/js/report-form.js
@@ -1,3 +1,4 @@
+console.log("script loaded")
 const description = document.getElementById('description');
 const remainingCharsText = document.getElementById('remaining-chars');
 const MAX_Chars = 2500;
@@ -20,4 +21,34 @@ if(description != null){
 }
 else{
    console.log("doesnt work")
+}
+
+//File upload script
+var fileUpload = document.getElementById("fileUpload");
+function listFiles() {
+   var files = fileUpload.files;
+   const giveTitle = document.createElement("p");
+   const node = document.createTextNode("Add descriptive titles to files below");
+   giveTitle.appendChild(node);
+   fileSection.appendChild(giveTitle);
+   for (var i=0; i < files.length; i++) {
+      var f = files[i];
+      console.log(f.name);
+      var fileName = f.name;
+      var fileExt = fileName.split(".")
+      const fileSection = document.getElementById("fileSection");
+      var titleInput = document.createElement("INPUT")
+      titleInput.setAttribute("type", "text");
+      titleInput.setAttribute("value", f.name.substring(0, f.name.lastIndexOf('.')));
+      fileSection.appendChild(titleInput);
+      var submitFileNames = document.createElement('input');
+      submitFileNames.setAttribute('type' , 'button');
+      submitFileNames.setAttribute('value', 'Submit new names');
+      fileSection.appendChild(submitFileNames);
+      btn.addEventListener("click", "updateFiles");
+   }
+}
+
+function updateFiles() {
+   alert("updating files")
 }
