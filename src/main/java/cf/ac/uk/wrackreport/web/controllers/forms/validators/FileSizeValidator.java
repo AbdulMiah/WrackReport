375998a1c750fc02888968380d1cad479aaff0d9
c2061455 <okeefesj@cardiff.ac.uk>
2021-11-26 01:10:37 +0000

    Added unit testing and tidied code

--- src/main/java/cf/ac/uk/wrackreport/web/controllers/forms/validators/FileSizeValidator.java
+++ src/main/java/cf/ac/uk/wrackreport/web/controllers/forms/validators/FileSizeValidator.java
@@ -5,23 +5,22 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.ArrayList;
-import java.util.List;
 
 public class FileSizeValidator implements ConstraintValidator<ValidFileSize, ArrayList<MultipartFile>> {
 
-    //    Phone number is valid of nothing entered, checks length if number is entered
     @Override
     public boolean isValid(ArrayList<MultipartFile> files, ConstraintValidatorContext context) {
         //validate number of files
         System.out.println("size: " + files.size());
         if (files.size() > 5) {
+            //Update message if too many files
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate("A maximum of 5 files are permitted").addConstraintViolation();
             return false;
         }
-//        Loop through files arraylist
+        //Loop through files arraylist
         for (MultipartFile f: files) {
-//            reject if file over 150mb
+            //reject if file over 150mb
             if (f.getSize() / 1024 > 150000) {
                 return false;
             }
