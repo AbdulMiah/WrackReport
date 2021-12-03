50c3154dc0d0ed6ee67d6245bebc1c30f69a8fb5
Taylor Harrison <harrisont7@cardiff.ac.uk>
2021-12-03 11:49:26 +0000

    Created an SQL view to view data for report-overview page

--- src/main/java/cf/ac/uk/wrackreport/web/controllers/OverviewController.java
+++ src/main/java/cf/ac/uk/wrackreport/web/controllers/OverviewController.java
@@ -1,13 +1,20 @@
 package cf.ac.uk.wrackreport.web.controllers;
 
+import cf.ac.uk.wrackreport.service.ReportService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OverviewController {
+private ReportService reportService;
+public OverviewController(ReportService reportService){
+    this.reportService = reportService;
+}
 
-    @GetMapping({"/", "reports-overview"})
-    public String displayOverview() {
+    @GetMapping({"/reports-overview"})
+    public String displayOverview(Model model) {
+        model.addAttribute("allReports", reportService.findAllReports());
         return "reports-overview.html";
 
     }
