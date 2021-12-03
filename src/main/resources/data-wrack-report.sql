081383cc15109e5c18eb640eaa420e3cb2695e67
c2061455 <okeefesj@cardiff.ac.uk>
2021-12-03 19:33:15 +0000

    Added more test data and updated tests

--- src/main/resources/data-wrack-report.sql
+++ src/main/resources/data-wrack-report.sql
@@ -16,7 +16,7 @@ TRUNCATE TABLE `depth_categories`;
 
 INSERT INTO `staff_users`
 VALUES(NULL, 'ROLE_STAFF', 'john', 'jones', 'john@gmail.com', "$2a$10$g4qzJA3h7.7vTfcxrBtV6eaR9TyhP.C6wbKT8zmAPrbibHp8riw8C", true);
-INSERT INTO `users`
+INSERT INTO `staff_users`
 VALUES(NULL, 'ROLE_ADMIN', 'admin', 'user', 'admin@gmail.com', "$2a$10$J.n9gEYayF9kTfQsqTT5u.KjH7JTpU57jImc1sX0/Em38kzHgTYoK", true);
 
 INSERT INTO `users`
@@ -33,6 +33,16 @@ INSERT INTO `users`
 VALUES(8, 'ROLE_USER', 'Tom', 'Brent', 'tombrent123@gmail.com', NULL, NULL, true);
 INSERT INTO `users`
 VALUES(9, 'ROLE_USER', 'Terry', 'Tibbs', 'thetibbs@gmail.co.uk', "07678874514", NULL, true);
+INSERT INTO `users`
+VALUES(12, 'ROLE_USER', 'Tommy', 'Taylor', 'tommyt@gmail.com', NULL, NULL, true);
+INSERT INTO `users`
+VALUES(13, 'ROLE_USER', 'Gemma', 'Banks', 'gbanks123@gmail.com', "07745667689", NULL, true);
+INSERT INTO `users`
+VALUES(14, 'ROLE_USER', 'John', 'Jenkins', 'jenkins_j@gmail.com', NULL, NULL, true);
+INSERT INTO `users`
+VALUES(15, 'ROLE_USER', 'Stacey', 'Chapman', 'staceyc@gmail.co.uk', "07665489765", NULL, true);
+INSERT INTO `users`
+VALUES(16, 'ROLE_USER', 'Steve', 'Wallace', 'swalls7@gmail.com', NULL, NULL, true);
 
 INSERT INTO `categories`
 VALUES(NULL, 'sewer flooding'); 
@@ -74,6 +84,16 @@ INSERT INTO `reports`
 VALUES (NULL, 8, 2, "River has completely overflowed and the flooding has reached the park", 1, 0.3456, "51.744038, -3.378296", "2021-11-29  14:17:00", NULL);
 INSERT INTO `reports`
 VALUES (NULL, 9, 4, "Lots of rubbish washed up near my shop from flood on 01/12/2021", 0, 0.3456, "51.500194, -3.562317", "2021-12-01  13:25:00", NULL);
+INSERT INTO `reports`
+VALUES (NULL, 12, 4, "Plastic waste left behind after flooding", 6, 0, "53.131942, -4.265442", "2021-11-29  19:50:00", NULL);
+INSERT INTO `reports`
+VALUES (NULL, 13, 5, "Very bad flooding from storm", 2, 0.25, "52.218966, -3.931732", "2021-10-13  13:00:00", NULL);
+INSERT INTO `reports`
+VALUES (NULL, 14, 4, "Debris washed up from river", 6, 0.05, "52.14908, -3.413315", "2021-09-09  14:00:00", NULL);
+INSERT INTO `reports`
+VALUES (NULL, 15, 1, "Sewer flooding - drains in street are overflowed", 1, 0.1, "52.717163, -3.696899", "2021-11-11  19:00:00", NULL);
+INSERT INTO `reports`
+VALUES (NULL, 16, 3, "Coastal flooding has reached some holiday homes on the beach", 3, 0.5, "51.650061, -4.761457", "2021-06-16  06:00:00", NULL);
 
 INSERT INTO `media`
 VALUES(NULL, 7, NULL, "debris", 1, "./test-data/LLFE0kQn10CVS9DaK7nu.jpeg");
@@ -87,6 +107,10 @@ INSERT INTO `media`
 VALUES(NULL, 11, NULL, "benches partly covered", 1, "./test-data/2mh7Yyq5mB2Hx75BFofV.jpg");
 INSERT INTO `media`
 VALUES(NULL, 12, NULL, "rubbish", 1, "./test-data/qW1t93BTJD4jyv7zDqtk.jpg");
+INSERT INTO `media`
+VALUES(NULL, 13, NULL, "coastal flooding", 1, "./test-data/2imVP6Sy5shUMUBoTOiH.jpg");
+
+
 
 INSERT INTO `metadata`
 VALUES(NULL, NULL, NULL, NULL);
--- src/test/java/cf/ac/uk/wrackreport/jpa/MediaJPATests.java
+++ src/test/java/cf/ac/uk/wrackreport/jpa/MediaJPATests.java
@@ -2,6 +2,7 @@ package cf.ac.uk.wrackreport.jpa;
 
 import cf.ac.uk.wrackreport.data.jpa.entities.MediaEntity;
 import cf.ac.uk.wrackreport.data.jpa.entities.ReportEntity;
+import cf.ac.uk.wrackreport.data.jpa.entities.UserEntity;
 import cf.ac.uk.wrackreport.data.jpa.repositories.MediaRepository;
 import cf.ac.uk.wrackreport.data.jpa.repositories.ReportRepository;
 import org.junit.jupiter.api.Test;
@@ -29,15 +30,17 @@ public class MediaJPATests {
     @Autowired
     MediaRepository mediaRepository;
 
-//    @Test
-//    public void shouldGetOneMedia() throws Exception {
-//        List<MediaEntity> media = new ArrayList<MediaEntity>();
-//        MediaEntity testMedia = new MediaEntity(null,null,"testMedia",1,"testpath");
-//        media.add(testMedia);
-//        ReportEntity reportEntity = new ReportEntity(null, 1L, (short)2, "test desc", (short)2, null, "51.896156,-3.933956", "2021-11-19 22:20:00", "CF24 4LR", media);
-//        reportRepository.save(reportEntity);
-//        List<MediaEntity> res = mediaRepository.findAll();
-//        assertEquals(1, res.size());
-//    }
+    @Test
+    public void shouldGetEightMedia() throws Exception {
+        List<MediaEntity> media = new ArrayList<MediaEntity>();
+        UserEntity user = new UserEntity(null, "ROLE_USER", "firstname", "lastname", "test@gmail.com", "07888747748", null, true);
+        MediaEntity testMedia = new MediaEntity(null,null,"testMedia",1,"testpath");
+        media.add(testMedia);
+        ReportEntity reportEntity = new ReportEntity(null, user, (short)2, "test desc", (short)2, 0.2f, "51.896156,-3.933956", "2021-11-19 22:20:00", "CF24 4LR", media);
+        reportRepository.save(reportEntity);
+        List<MediaEntity> res = mediaRepository.findAll();
+        System.out.println(res);
+        assertEquals(8, res.size());
+    }
 
 }
