de3693a36e4bb439df41d758a142015ceef6c81c
Abdul Miah <miaham@cardiff.ac.uk>
2021-12-04 14:33:00 +0000

    Added API to search for reports by ID.

--- src/main/java/cf/ac/uk/wrackreport/api/controllers/ReportAPIController.java
+++ src/main/java/cf/ac/uk/wrackreport/api/controllers/ReportAPIController.java
@@ -1,15 +1,16 @@
 package cf.ac.uk.wrackreport.api.controllers;
 
-import cf.ac.uk.wrackreport.data.jpa.entities.ReportEntity;
-import cf.ac.uk.wrackreport.data.jpa.repositories.ReportRepository;
+import cf.ac.uk.wrackreport.api.exceptions.ReportNotFoundAPIException;
 import cf.ac.uk.wrackreport.service.ReportService;
 import cf.ac.uk.wrackreport.service.dto.ReportDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api")
@@ -27,5 +28,17 @@ public class ReportAPIController {
         reportDTOList = reportService.findAllReports();
 
         return ResponseEntity.ok(reportDTOList);
+    }
+
+    @GetMapping("/report/{furl}")
+    public ResponseEntity<?> findByFurl(@PathVariable(value = "furl", required = true) Long reportId) {
+        Optional<ReportDTO> reportDTO;
+        reportDTO = reportService.findByReportId(reportId);
+
+        if (reportDTO.isPresent()) {
+            return ResponseEntity.ok(reportDTO.get());
+        } else {
+            throw new ReportNotFoundAPIException("That report does not exist.");
+        }
     }
 }
