3b86906b4eb049c4e16c7c34ec35de18d5f45fd0
Abdul Miah <miaham@cardiff.ac.uk>
2021-12-04 13:11:42 +0000

    Changed map on detailed report page to the admin map

--- src/main/java/cf/ac/uk/wrackreport/web/controllers/DetailedReportController.java
+++ src/main/java/cf/ac/uk/wrackreport/web/controllers/DetailedReportController.java
@@ -1,5 +1,6 @@
 package cf.ac.uk.wrackreport.web.controllers;
 
+import cf.ac.uk.wrackreport.service.ReportService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DetailedReportController {
 
+    private ReportService reportService;
+
+    public DetailedReportController(ReportService aReportService) {
+        this.reportService = aReportService;
+    }
+
     @GetMapping("/detailed-report")
     public String viewDetailedReport(Model model) {
+        model.addAttribute("reports", reportService.findAllReports());
         return "detailed-report";
     }
 }
