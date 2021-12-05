53cc019a468471eb791519d5c15a03d7ab2773f6
c2061455 <okeefesj@cardiff.ac.uk>
2021-12-05 00:09:04 +0000

    Added report query form class

---
+++ src/main/java/cf/ac/uk/wrackreport/web/controllers/forms/ReportQueryForm.java
@@ -0,0 +1,22 @@
+package cf.ac.uk.wrackreport.web.controllers.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportQueryForm {
+
+    private String postcode;
+
+    private String localAuthority;
+
+    private short categoryId;
+
+    private String dateFrom;
+
+    private String dateTo;
+
+}
