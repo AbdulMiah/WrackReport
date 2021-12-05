b0e3d89bcc98d925f6bc6d38de74f7bb417cc2ed
c2061455 <okeefesj@cardiff.ac.uk>
2021-12-05 16:57:34 +0000

    Added date to and from to stored procedure

--- src/main/java/cf/ac/uk/wrackreport/service/ReportOverviewService.java
+++ src/main/java/cf/ac/uk/wrackreport/service/ReportOverviewService.java
@@ -1,9 +1,14 @@
 package cf.ac.uk.wrackreport.service;
 
 import cf.ac.uk.wrackreport.service.dto.ReportOverviewDTO;
+import org.springframework.data.jpa.repository.query.Procedure;
 
 import java.util.List;
 
 public interface ReportOverviewService {
     List<ReportOverviewDTO> findAllReportOverview();
+
+    @Procedure("ReportQuery")
+    int reportQuery(String model);
+
 }
