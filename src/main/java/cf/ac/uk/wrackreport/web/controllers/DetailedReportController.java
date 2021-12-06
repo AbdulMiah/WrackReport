fe178e1cf9afd67d72ba9f0155e31aa708298749
Abdul Miah <miaham@cardiff.ac.uk>
2021-12-06 15:09:26 +0000

    Added validation to check user roles. If user role is admin or staff, the contact details appear in the detailed report page. If user role is not admin or staff, then hide contact details

--- src/main/java/cf/ac/uk/wrackreport/web/controllers/DetailedReportController.java
+++ src/main/java/cf/ac/uk/wrackreport/web/controllers/DetailedReportController.java
@@ -4,10 +4,13 @@ import cf.ac.uk.wrackreport.service.DetailedReportService;
 import cf.ac.uk.wrackreport.service.ReportService;
 import cf.ac.uk.wrackreport.service.dto.DetailedReportDTO;
 import cf.ac.uk.wrackreport.service.dto.ReportDTO;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Optional;
 
@@ -25,8 +28,17 @@ public class DetailedReportController {
         Optional<DetailedReportDTO> detailedReportDTO;
         detailedReportDTO = detailedReportService.findAllByReportId(reportId);
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication.getAuthorities());
+
+        SimpleGrantedAuthority adminAuth = new SimpleGrantedAuthority("ROLE_ADMIN");
+        SimpleGrantedAuthority staffAuth = new SimpleGrantedAuthority("ROLE_STAFF");
+
+        Boolean auth = authentication.getAuthorities().contains(adminAuth) || authentication.getAuthorities().contains(staffAuth);
+
         if (detailedReportDTO.isPresent()) {
             model.addAttribute("report", detailedReportDTO.get());
+            model.addAttribute("authenticated", auth);
             return "detailed-report";
         } else {
             return "redirect:/";
