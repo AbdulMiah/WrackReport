79a1bb97987bdd1002cee8dc259d49be70587295
AbdulMiah <abdulmuktadirmiah@gmail.com>
2021-12-08 17:05:22 +0000

    Added a redirect from HTTP (port 8080) to HTTPS (port 8443)

--- src/main/java/cf/ac/uk/wrackreport/security/WebSecurityConfig.java
+++ src/main/java/cf/ac/uk/wrackreport/security/WebSecurityConfig.java
@@ -6,14 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {//extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserDetailsService userDetailsService;
@@ -45,24 +46,23 @@ public class WebSecurityConfig {//extends WebSecurityConfigurerAdapter {
 //                                .permitAll());
 //    }
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .requiresChannel(channel ->
-                        channel.anyRequest().requiresSecure())
-                .authorizeRequests(authorize ->
-                        authorize.anyRequest().permitAll())
-                .build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
     }
 
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/**")
-//                .permitAll();
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+//                .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
+//                .build();
 //    }
 
     //Create Authentication Manager Builder to configure authentication
-//    @Override
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
     }
