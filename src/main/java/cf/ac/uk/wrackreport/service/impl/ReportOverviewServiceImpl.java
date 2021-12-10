48653ab6a1280124e0f89a543de58e5544b0f78c
c2061455 <okeefesj@cardiff.ac.uk>
2021-12-10 17:35:12 +0000

    Converting native query to jpql

--- src/main/java/cf/ac/uk/wrackreport/service/impl/ReportOverviewServiceImpl.java
+++ src/main/java/cf/ac/uk/wrackreport/service/impl/ReportOverviewServiceImpl.java
@@ -6,6 +6,7 @@ import cf.ac.uk.wrackreport.service.dto.ReportOverviewDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
