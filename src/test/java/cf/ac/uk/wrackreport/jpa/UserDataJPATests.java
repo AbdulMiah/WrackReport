db73522a000a1e51c8a456c82057e9ad5debbfd5
c2061455 <okeefesj@cardiff.ac.uk>
2021-12-11 17:07:35 +0000

    Added test for user being linked to report

--- src/test/java/cf/ac/uk/wrackreport/jpa/UserDataJPATests.java
+++ src/test/java/cf/ac/uk/wrackreport/jpa/UserDataJPATests.java
@@ -1,6 +1,9 @@
 package cf.ac.uk.wrackreport.jpa;
 
+import cf.ac.uk.wrackreport.data.jpa.entities.MediaEntity;
+import cf.ac.uk.wrackreport.data.jpa.entities.ReportEntity;
 import cf.ac.uk.wrackreport.data.jpa.entities.UserEntity;
+import cf.ac.uk.wrackreport.data.jpa.repositories.ReportRepository;
 import cf.ac.uk.wrackreport.data.jpa.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,6 +24,9 @@ public class UserDataJPATests {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    ReportRepository reportRepository;
 
     @Test
     public void shouldGetTwelveUsers() throws Exception{
@@ -33,6 +40,23 @@ public class UserDataJPATests {
         userRepository.save(newUser);
         List<UserEntity> userList = userRepository.findAll();
         assertEquals(13, userList.size());
+    }
+
+    @Test
+    public void userIsLinkedToReport() throws Exception {
+        // Given new report with user
+        UserEntity testUser = new UserEntity(null, "ROLE_USER", "test", "user", "test@gmail.com", "08847567364", "testpass", true);
+        List<MediaEntity> media = new ArrayList<MediaEntity>();
+        MediaEntity testMedia = new MediaEntity(null,null,null,"testMedia",1,"testpath");
+        media.add(testMedia);
+        ReportEntity testReport = new ReportEntity(null, testUser, (short)2, "test desc", (short)2, 0.2f, "51.896156,-3.933956", "2021-11-19 22:20:00", "CF24 4LR", "Cardiff", 0, media);
+
+        // When report saved to database
+        ReportEntity insertedReport = reportRepository.save(testReport);
+
+        // User is linked to report
+        UserEntity insertedUser = insertedReport.getUser();
+        assertEquals("test", insertedUser.getFirstName());
     }
 
 }
