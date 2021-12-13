b164d728fc8741b1a1e1846bbcad0a45356ccf49
AbdulMiah <abdulmuktadirmiah@gmail.com>
2021-12-13 12:20:55 +0000

    Added logging to the error controller. Helpful for debugging when encoutering an error

--- src/main/java/cf/ac/uk/wrackreport/web/controllers/GeneralErrorController.java
+++ src/main/java/cf/ac/uk/wrackreport/web/controllers/GeneralErrorController.java
@@ -1,14 +1,33 @@
 package cf.ac.uk.wrackreport.web.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @Controller
 public class GeneralErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError() {
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                log.debug("ERROR 404: " + HttpStatus.NOT_FOUND.toString());
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                log.debug("ERROR 403: " + HttpStatus.FORBIDDEN.toString());
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                log.debug("ERROR 500: " + HttpStatus.INTERNAL_SERVER_ERROR.toString());
+            }
+        }
         return "error";
     }
 }
